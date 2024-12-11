1fd3021c2003c39478b30c55d3c76d474e689a73
pennapha1233 <147477065+pennapha1233@users.noreply.github.com>
2024-12-11 16:37:54 +0700

    Update Example02_06.java

--- chapter2/src/Example02_06.java
+++ chapter2/src/Example02_06.java
@@ -1,6 +1,26 @@
 public class Example02_06 {
     long id = 6606021610206L;
     String name = "Pennapha";
-    S
-    
+    String surname = "Kamapate";
+    int age = 20;
+
+    public Example02_06() { // constrator method
+    }
+
+    public static void main(String[] args) {
+        String info;
+        Example02_06 person = new Example02_06(); // create object
+        info = person.name + " " + person.surname;
+        info += ", " + person.age + " year old.";
+        System.out.println("ID : "+ person.id);
+        System.out.println("Data person : " + info);
+        System.out.println("======================================");
+        person.name = "John";
+        person.surname = "Smith";
+        person.age = 25;
+        info = person.name + " " + person.surname;
+        info += ", " + person.age + " year old.";
+        System.out.println("ID : " + person.id);
+        System.out.println("New data person : " + info);
+    }
 }
